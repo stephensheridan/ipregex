4a5d6b454cf59e3593a1aca5f4f667d02cdc4155
Stephen Sheridan <stesher@gmail.com>
2016-04-26 17:18:35 +0100

    Assume one IP and one timestamp per line so we can simplify code a litte. Added check to make sure we have IP and timestamp. Also made a nicer looking date out of the Apache log file date.

--- logparse.py
+++ logparse.py
@@ -1,7 +1,7 @@
 import re
 import os
 import geoip2.database
-
+from datetime import datetime
 
 # Path to the GeoLite database
 geodb = "/GeoLite2-City.mmdb"
@@ -20,20 +20,25 @@ fout = open("iplocation.csv", 'w')
 with open(logfile) as fp:
   # For each line in the Apache log file
   for line in fp:
-    # Get a list of IP matches on this line
-    iplist = re.findall( r'[0-9]+(?:\.[0-9]+){3}', line )
-    # Get the first timestamp from the line
+    # Get a list of IP matches on this line [:1] gives us first item in list
+    iplist = re.findall( r'[0-9]+(?:\.[0-9]+){3}', line )[:1]
+    # Get the first timestamp from the line [:1] gives us first item in list
     stamp = re.findall(r'\[(.*?)\]',line)[:1]
-    # For each IP match in the list
-    for ip in iplist:
+    # If we have a timestamp and ip address NOTE: once they are not empty lists
+    if (iplist and stamp):
       try:
+        # Make a nicer timestamp for the file - strip off timezone
+        dstamp = stamp[0]
+        dstamp = dstamp[0:dstamp.find(' ')]
+        # Format the date to YYYY-MM-DD HH:MM:SS
+        dstamp = datetime.strptime(dstamp,"%d/%b/%Y:%H:%M:%S")
         # Try to get a response back for the IP address
-        response = reader.city(ip)
+        response = reader.city(iplist[0])
         # Just do some output to make sure something is happening
-        print response.country.name + " " + response.city.name
+        print str(dstamp) + " " + str(iplist[0]) + " " + response.country.name + " " + response.city.name
         # Write out the stamp,IP, Country, City
-        fout.write(str(stamp[0]) + "," + str(ip) + "," + str(response.country.name) + "," + str(response.city.name + "\n"))
+        fout.write(str(dstamp) + "," + str(iplist[0]) + "," + str(response.country.name) + "," + str(response.city.name + "\n"))
       except Exception as e:
-        print "Cannot process IP address: " + str(ip)
+        print "Cannot process IP address: " + str(iplist[0])
 
 fout.close()
